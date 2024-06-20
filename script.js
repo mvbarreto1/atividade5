39a83ef4dc13df212584f2b503c479d94a4b5f98
mvbarreto1 <marcusbarreto@outlook.com.br>
2024-06-20 12:07:56 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,25 @@
+let num 1 = 7
+switch (numero){
+case 1:
+    console.log ('segunda-feira')
+    break
+case 2:
+    console.log ('terça-feira')
+    break
+case 3:
+    console.log ('quata-feira')
+    break
+case 4:
+    console.log ('quinta-feira')
+    break
+case 5:
+    console.log ('sexta-feira')
+     break
+case 6:
+     console.log ('sabado')
+     break
+case 7:
+    console.log ('domingo')
+    break    
+
+}
